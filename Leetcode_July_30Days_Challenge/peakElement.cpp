d775adc3488369aaef7100f1cf3530a602490201
Vartika Rana <vartikarana2000@gmail.com>
2021-07-13 23:32:46 +0530

    create peakElement

---
+++ Leetcode_July_30Days_Challenge/peakElement.cpp
@@ -0,0 +1,21 @@
+//O(logn)
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int low=0, high= nums.size()-1;
+        while(low<=high){
+            int mid= (low+high)/2;
+            if((mid==0 || nums[mid]>=nums[mid-1]) && (mid==nums.size()-1 || nums[mid]>=nums[mid+1]))
+                return mid;
+            else
+            {
+                if(mid>0 && nums[mid] <= nums[mid-1])
+                    high= mid-1;
+                else
+                    low= mid+1;
+            }
+        }
+        return -1;
+    }
+};
