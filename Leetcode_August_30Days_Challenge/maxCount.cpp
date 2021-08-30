a4bebd5670abadc5475f7f71ff1e04695957dc8f
Vartika Rana <vartikarana2000@gmail.com>
2021-08-30 13:39:14 +0530

    create macCount

---
+++ Leetcode_August_30Days_Challenge/maxCount.cpp
@@ -0,0 +1,21 @@
+//O(size(ops)) time
+
+class Solution {
+public:
+    int maxCount(int m, int n, vector<vector<int>>& ops) {
+        int commonRows = m, commonCols = n;
+        for(int i=0;i<ops.size();i++)
+        {
+            if(ops[i][0] < commonRows && ops[i][1] < commonCols)
+            {
+                commonRows = ops[i][0];
+                commonCols = ops[i][1];
+            }
+            else if(ops[i][0] < commonRows)
+                commonRows = ops[i][0];
+            else if(ops[i][1] < commonCols)
+                commonCols = ops[i][1];
+        }
+        return commonRows*commonCols;
+    }
+};
